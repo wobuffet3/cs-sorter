4a00640915577ed691b07840b36508bc5575a20d
wobuffet3 <kyu_bey@yahoo.com>
2024-04-17 16:51:13 -0400

    Update and rename 2021-05-09.js to 2024-04-17.js
    initial dataset

--- src/js/data/2024-04-17.js
+++ src/js/data/2024-04-17.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2021-05-09"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2024-04-17"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
